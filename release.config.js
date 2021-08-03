35d9819b6ad2e752def66e53920dbcb682bf91cc
Filip Cuic <zgchuck@gmail.com>
2021-08-03 10:42:53 +0200

    fix: dweef

--- release.config.js
+++ release.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/ChuckPhilips/react-app",
+  plugins: [
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
 };
