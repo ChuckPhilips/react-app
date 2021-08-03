d82110a76c725b8f149a30916102c7cdd4818e52
Filip Cuic <zgchuck@gmail.com>
2021-08-03 07:50:25 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-	  Dinamo
-          Learn React
+          Dinamo Learn React
         </a>
       </header>
     </div>
